bd36c430c2767a337801aca4cf31e5857da14987
jase <jase@bikon.jp>
2017-01-09 19:24:54 +1100

    Remove imports from ExampleServiceTest

--- src/test/java/jp/bikon/service/ExampleServiceTest.java
+++ src/test/java/jp/bikon/service/ExampleServiceTest.java
@@ -1,10 +1,8 @@
 package jp.bikon.service;
 
 import jp.bikon.model.Example;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -20,7 +18,6 @@ import static org.junit.Assert.assertNull;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ExampleServiceTest {
 
     @Autowired
@@ -40,16 +37,18 @@ public class ExampleServiceTest {
         assertEquals(read.getMessage(), example.getMessage());
 
         // Update
-        final Example updated = new Example("12345", "Updated message");
-        service.update(updated);
+        example.setMessage("Updated message");
+        service.update(example);
+
         // Verify update
         final Example readUpdated = service.getById("12345");
         assertNotNull("Should not be null", readUpdated);
-        assertEquals(updated.getId(), readUpdated.getId());
-        assertEquals(updated.getMessage(), readUpdated.getMessage());
+        assertEquals(example.getId(), readUpdated.getId());
+        assertEquals(example.getMessage(), readUpdated.getMessage());
 
         // Delete
         service.delete("12345");
+
         // Verify delete
         final Example deleted = service.getById("12345");
         assertNull("Should be null", deleted);
